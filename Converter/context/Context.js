4b344e825fbecede95770d7ee0c15556523c5299
danielkoh99 <48031948+danielkoh99@users.noreply.github.com>
2021-04-02 12:44:53 +0200

    Delete Converter/context directory

--- Converter/context/Context.js
+++
@@ -1,7 +0,0 @@
-import {createContext, useState} from 'react';
-
-export const State = createContext(false);
-
-// const StateProvider = () => {
-//   const [state, setstate] = useState(initialState);
-// };
